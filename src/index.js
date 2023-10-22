17439a836b176d125eb19cba3e46f63cea2778ed
Cris3h <cris.treachi@gmail.com>
2023-10-22 02:17:09 -0300

    fix: BrowserRouter -> HashRouter

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 import { onRender } from "./test";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-  <Profiler id='App' onRender={onRender}>
+  // <BrowserRouter>
+  // <Profiler id='App' onRender={onRender}>
 
-    <App />
-  </Profiler>
-  </BrowserRouter>
+  //   <App />
+  // </Profiler>
+  // </BrowserRouter>
+
+  <HashRouter>
+    <Profiler id="App" onRender={onRender}>
+      <App />
+    </Profiler>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
